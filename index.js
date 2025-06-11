19a0a7a65c23f2c69cb01a927d5fa211d0585597
Hinal <hinalnagar95@gmail.com>
2025-06-11 23:21:50 +0530

    index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { videoRoute } from './routes/video.route.js';
 
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173',
+app.use(cors({ origin: 'https://youtube-clone-26rh.vercel.app',
      credentials: true }));
 dotenv.config();
 app.use(express.json());
